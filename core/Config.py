32fe73fb438adab8740e7308a35c49269bb61679
Tom <tom@pi>
2021-01-21 20:27:06 +0200

    FIX: made away with local pos.

--- core/Config.py
+++ core/Config.py
@@ -6,7 +6,7 @@ class Config(metaclass=Singleton):
     def __init__(self):
         self.cfg = {}
 
-        with open("conf.yml", 'r') as stream:
+        with open("/share/wlc/conf.yml", 'r') as stream:
             try:
                 self.cfg = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# from MotionDetector import MotionDetector
-# from FeroxListener import FeroxListener
-# from core.Logger import Logger
-# import threading
-# from MotionDetectorThreadHandler import MotionDetectorThreadHandler
+from MotionDetector import MotionDetector
+from FeroxListener import FeroxListener
+from core.Logger import Logger
+import threading
+from MotionDetectorThreadHandler import MotionDetectorThreadHandler
 
-# Logger().logger.info("Started Ferox")
+Logger().logger.info("Started Ferox")
 
-# def listen():
-#     FeroxListener().start_polling()
+def listen():
+    FeroxListener().start_polling()
 
-# threading.Thread(target=listen, args=()).start()
-import vcgencmd
-CPUc=vcgencmd.measure_temp()
-
-print(CPUc)
+threading.Thread(target=listen, args=()).start()
