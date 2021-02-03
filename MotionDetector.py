c6d0e9dc964a23c270ccb33c5afb05aa4a712e1d
Tom <tom@pi>
2021-02-03 22:29:46 +0200

    removed unneccessary logging

--- MotionDetector.py
+++ MotionDetector.py
@@ -27,8 +27,6 @@ class MotionDetector:
             img1 = self.cam.Capture()
             img2 = self.cam.Capture()
 
-            Logger().logger.info("img")
-
             if(self.handler.sendLatest):
                 self.handler.sendLatest = False
 
