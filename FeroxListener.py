c4165062eda8669e3b90e8ac3198d0ed6d659d2e
Tom <tom@pi>
2021-02-03 22:25:26 +0200

    FIX: Use correct conf

--- FeroxListener.py
+++ FeroxListener.py
@@ -29,7 +29,7 @@ class FeroxListener:
     def update(self, update, context):
         import subprocess
         import sys
-        subprocess.Popen("sleep 5 && sh %s &> %s" % (Config().get()['Logger']['Path'], Config().get()['Logger']['Path']), shell=True)
+        subprocess.Popen("sleep 5 && sh %s &> %s" % (Config().get()['Updater']['Path'], Config().get()['Logger']['Path']), shell=True)
         sys.exit(0)
 
     def start_polling(self):
