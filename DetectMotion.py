3eecfcd33a1f0f5e595264e37d5c729cb53d7ebd
Tom <tom.yrjas@netikka.fi>
2021-03-12 13:41:32 +0200

    Decreased minimum contourArea x3

--- DetectMotion.py
+++ DetectMotion.py
@@ -22,7 +22,7 @@ class DetectMotion:
         nonZero = cv2.countNonZero(dilate_image)
 
         for contour in contours:
-            if cv2.contourArea(contour) < 20: continue
+            if cv2.contourArea(contour) < 50: continue
 
             contourCount = contourCount + 1
 
